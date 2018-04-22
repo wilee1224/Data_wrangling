f45c4403ef4cce7b43c9dfc2aab53ef77b85d96d
wilee1224 <wilee1224@gmail.com>
2018-04-22 21:21:12 +0900

    엑셀 파일 commit

--- parse_excel.py
+++ parse_excel.py
@@ -5,15 +5,23 @@ sheet = book.sheet_by_name('Table 9 ')
 
 # for sheet in book.sheet():
 # print sheet.nrows
-count = 0
+
 data = {}
 
-for i in xrange(sheet.nrows):
-    if count < 20:
-        if i >= 14:
-            row = sheet.row_values(i)
-            country = row[1]
-            data[country] = {}
-    count += 1
+for i in xrange(14, sheet.nrows):
+    row = sheet.row_values(i)
+    country = row[1]
 
-print data
+    data[country] = {
+        'child_labor' : {
+            'total' : [],
+            'male' : [],
+            'female' : [],
+        },
+        'child_marriage' : {
+            'marriage_by_15' : [],
+            'marriage_by_18' : [],
+        }
+    }
+
+print data['Afghanistan']
