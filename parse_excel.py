54c470919a64240b2679fc8663bbe2912a9d8997
wilee1224 <wilee1224@gmail.com>
2018-04-22 21:27:58 +0900

    parse_excel 중간 업로드(page 93)

--- parse_excel.py
+++ parse_excel.py
@@ -14,13 +14,13 @@ for i in xrange(14, sheet.nrows):
 
     data[country] = {
         'child_labor' : {
-            'total' : [],
-            'male' : [],
-            'female' : [],
+            'total' : [row[4], row[5]],
+            'male' : [row[6], row[7]],
+            'female' : [row[8], row[9]],
         },
         'child_marriage' : {
-            'marriage_by_15' : [],
-            'marriage_by_18' : [],
+            'marriage_by_15' : [row[10], row[11]],
+            'marriage_by_18' : [row[12], row[13]],
         }
     }
 
