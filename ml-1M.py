606b87d12f108ef709b7e245901e43bd24763dee
Woo Il, Lee <wilee1224@gmail.com>
2018-08-05 23:05:58 +0900

    Update ml-1M.py

--- ml-1M.py
+++ ml-1M.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
-encoding = 'latin1'
+import sys
+reload(sys)
+# encoding = 'latin1'
 
 upath = os.path.expanduser('users.dat')
 rpath = os.path.expanduser('ratings.dat')
@@ -13,3 +15,6 @@ mnames = ['movie_id', 'title', 'genres']
 users = pd.read_csv(upath, sep='::', header=None, names=unames, encoding=encoding, engine='python')
 ratings = pd.read_csv(rpath, sep='::', header=None, names=rnames, encoding=encoding, engine='python')
 movies = pd.read_csv(mpath, sep='::', header=None, names=mnames, encoding=encoding, engine='python')
+
+# mean_ratings = mean_ratings = data.pivot_table('rating', index='title', columns='gender', aggfunc='mean')
+# data = pd.merge(pd.merge(ratings, users), movies)
