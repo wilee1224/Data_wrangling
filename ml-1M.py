036b2f3009ec984e1583582acbc4e9c56cb95222
wilee1224 <wilee1224@gmail.com>
2018-08-05 23:35:25 +0900

    page 51 교재 처음 성공 파일

--- ml-1M.py
+++ ml-1M.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import sys
 reload(sys)
+sys.setdefaultencoding('utf-8')
 # encoding = 'latin1'
 
 upath = os.path.expanduser('users.dat')
@@ -12,9 +13,10 @@ unames = ['user_id', 'gender', 'age', 'occupation', 'zip']
 rnames = ['user_id', 'movie_id', 'rating', 'timestamp']
 mnames = ['movie_id', 'title', 'genres']
 
-users = pd.read_csv(upath, sep='::', header=None, names=unames, encoding=encoding, engine='python')
-ratings = pd.read_csv(rpath, sep='::', header=None, names=rnames, encoding=encoding, engine='python')
-movies = pd.read_csv(mpath, sep='::', header=None, names=mnames, encoding=encoding, engine='python')
+users = pd.read_csv(upath, sep='::', header=None, names=unames, engine='python')
+ratings = pd.read_csv(rpath, sep='::', header=None, names=rnames, engine='python')
+movies = pd.read_csv(mpath, sep='::', header=None, names=mnames, engine='python')
 
-# mean_ratings = mean_ratings = data.pivot_table('rating', index='title', columns='gender', aggfunc='mean')
 # data = pd.merge(pd.merge(ratings, users), movies)
+# mean_ratings = mean_ratings = data.pivot_table('rating', index='title', columns='gender', aggfunc='mean')
+
